ec4f70b5005b3759deb7cad93a2cd02a7ad8eb60
Djuradj Perunovic <djuradj.perunovic@bild-studio.net>
2024-08-06 07:27:55 +0200

    Added composition api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 import { defineNuxtConfig } from '@nuxt/bridge'
 
 export default defineNuxtConfig({
+  // bridge: {
+  //   typescript: true,
+  //   nitro: true,
+  // },
   bridge: false,
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -30,12 +34,13 @@ export default defineNuxtConfig({
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
+  // buildModules: [
+  //   // https://go.nuxtjs.dev/typescript
+  //   '@nuxt/typescript-build',
+  //   // https://go.nuxtjs.dev/tailwindcss
+  //   '@nuxtjs/tailwindcss',
+  // ],
+
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -44,10 +49,10 @@ export default defineNuxtConfig({
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
+  // axios: {
+  //   // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+  //   baseURL: '/',
+  // },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
